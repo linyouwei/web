29df25603e763f8088dcc8ffd944563644e00144
linyouwei <yjlyw020150@163.com>
2018-06-30 17:22:49 +0800

    shop登陆

--- shop/run/src/PublicPage/login.js
+++ shop/run/src/PublicPage/login.js
@@ -2,10 +2,72 @@ $(function(){
     var getParams = getArgs();
     var shop = getParams.shop;
     var login = getParams.login;
-    console.log(shop);
-    console.log(login);
-    var originalhref = $('#header a').attr('href','/shop/PublicPage/center.html'+'?shop='+shop);
+    console.log('123:'+login);
+    var originalhref = $('#header a').attr('href');
     console.log(originalhref);
+    $('#header a').attr('href',originalhref+'?shop='+shop);
+    var originalhref = $('.register-btn a').attr('href');
+    console.log(originalhref);
+    $('.register-btn a').attr('href',originalhref+'?shop='+shop);
+    var originalhref = $('.forget-btn a').attr('href');
+    console.log(originalhref);
+    $('.forget-btn a').attr('href',originalhref+'?shop='+shop);
+    var storage = window.localStorage;
+    var today = new Date().getTime();
+  /*  $('#login-container').inputValidate({
+        submit: {
+            query: '#requestSubmit'
+        },
+        inputs: [{
+            query: '#requestPassword',
+            regexp: window.commonRegexp.password.regexp,
+            message: window.commonRegexp.password.message,
+            emptyMessage: window.commonRegexp.password.emptyMessage
+        }]
+    });*/
+    //账号密码登陆
+    $("#requestSubmit").click(function(){
+        var submitBtn = $("#requestSubmit");
+        var params = {};
+        params.mobile = $('#requestMobile').val();
+        params.password = $('#requestPassword').val();
+        params.areaCode = '86';
+        toggleLoading(true);
+    /*    ajaxRequest('http://booking.uclbrt.com/api/user/login', params, 'post', function (data) {
+            toggleLoading(false);
+            if (typeof storage === 'object') {
+                try {
+                    storage.id_shopCookie = data.data.id;
+                    storage.token_shopCookie = data.data.token;
+                    storage.token_time = today;
+                    window.location.href = '/shop/PublicPage/center.html?shop='+shop;
+                } catch (e) {
+                    Storage.prototype._setItem = Storage.prototype.setItem;
+                    Storage.prototype.setItem = function() {};
+                    alert(lang('traceless'));//  'traceless':'请关闭无痕浏览',
+                }
+            }
+        }, function (error, obj, defaultErrorHandler) {
+            toggleLoading(false);
+            defaultErrorHandler(error, obj);
+        }, submitBtn);*/
+
+      /*  var url = 'http://booking.uclbrt.com/api/user/login'
+        console.log(params);
+        $.ajax({
+            url:url,
+            data: params,
+            async:true,
+            type: 'post',
+            dataType: 'jsonp',
+            jsonp: "callbackparam",
+            crossDomain: true,
+            success: function (data) {
+                console.log(params);
+            }
+        });*/
+
+    });
 
 
 });
--- shop/run/src/common.js
+++ shop/run/src/common.js
@@ -1,3 +1,47 @@
+// 多语言方法
+var lang = function(tag) {
+    return window.languageResource[tag] || '';
+};
+$(function () {
+    $('body').inputAction({
+        global: true
+    }).height($(window).height());
+    // 常用正则
+    window.commonRegexp = {
+        'mobile': {
+            'regexp': /^1[34578]{1}\d{9}$/,
+            'emptyMessage': lang('common.02'),
+        },
+        'password': {
+            // 'regexp': /^(?=^[a-zA-Z0-9]{6,20}$).*[a-zA-Z]+.*$/,
+            'regexp': /^(?=.*?[a-zA-Z])(?=.*?[0-9])[a-zA-Z0-9]{6,20}$/,
+            'message': lang('common.03'),
+            'emptyMessage': lang('common.04'),
+        },
+        'imageCode': {
+            'regexp': /^[0-9a-zA-Z]{4}$/,
+            'message': lang('common.05'),
+            'emptyMessage': lang('common.06'),
+        },
+        'address': /^.{1,255}$/,
+        'vcode': {
+            'regexp': /^\d{4}$/,
+            'message': lang('common.07'),
+            'emptyMessage': lang('common.08'),
+        },
+        'smsvcode': {
+            'regexp': /^\w{6}$/,
+            'message': lang('common.07'),
+            'emptyMessage': lang('common.08'),
+        },
+        'username': {
+            'regexp': /^.{1,15}$/,
+            'message': lang('common.10'),
+            'emptyMessage': lang('common.11'),
+        }
+    };
+});
+
 var getArgs = function getArgs() {
         var args = {};
         var query = location.search.substring(1);
@@ -18,4 +62,69 @@ var getArgs = function getArgs() {
             // Store as a property
         }
         return args; // Return the object
+};
+
+// 显示或隐藏加载中动画
+/* exported toggleLoading */
+var toggleLoading = function (isShow) {
+    if (isShow) {
+        $('#loadingContainer').removeClass('hidden');
+        $('body').addClass('modal-open');
+    } else {
+        $('#loadingContainer').addClass('hidden');
+        $('body').removeClass('modal-open');
+    }
+};
+
+//ajax异步请求
+/* exported ajaxRequest */
+var ajaxRequest = function (url, params, method, successcallback, errorcallback, obj) {
+    if (method === null || method === '') {
+        method = 'get';
+    }
+    if (obj !== undefined) {
+        if (obj.attr('disabled') !== undefined) {
+            return false;
+        }
+        obj.attr('disabled', true);
+    }
+    $.ajax({
+        url: url,
+        data: params,
+        type: method,
+        cache: false,
+        dataType: 'json',
+        success: function (data) {
+            if (obj !== undefined) {
+                obj.removeAttr('disabled');
+            }
+            if (parseInt(data.error)>0) {
+                if (typeof errorcallback == 'function') {
+                    errorcallback(data, obj, defaultErrorHandler);
+                } else {
+                    defaultErrorHandler(data, obj);
+                }
+            } else {
+                if (typeof successcallback == 'function') {
+                    successcallback(data, obj);
+                } else {
+                    window.location.reload();
+                 /*   reload 方法，该方法强迫浏览器刷新当前页面。
+                       语法：location.reload([bForceGet])参数： bForceGet， 可选参数， 默认为 false，从客户端缓存里取当前页。 true, 则以GET 方式，从服务端取最新的页面, 相当于客户端点击 F5("刷新")
+                */
+                }
+            }
+        },
+        error: function (error) {
+            if (obj !== undefined) {
+                obj.removeAttr('disabled');
+            }
+            if (typeof errorcallback == 'function') {
+                errorcallback(error, obj, defaultErrorHandler);
+            } else {
+                defaultErrorHandler(error, obj);
+            }
+
+        }
+    });
 };
